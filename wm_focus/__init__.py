0a05604e9fa98797cdae10f63a3ac49146539ed4
jarasz.kevwargo <jarasz.kevwargo@gmail.com>
2023-10-15 18:49:11 +0200

    Add --class parameter

--- wm_focus/__init__.py
+++ wm_focus/__init__.py
@@ -1,4 +1,6 @@
 import sys
+from argparse import ArgumentParser
+from subprocess import Popen
 from subprocess import run as run_cmd
 
 CONFIG = {
@@ -46,14 +48,32 @@ CONFIG = {
 
 
 def run():
-    window = CONFIG[sys.argv[1]]
-    search_args = ["--class", window["class"]] if window.get("class") else ["--name", window["name"]]
+    args = parse_args()
+    if alias := args.alias:
+        window = CONFIG[alias]
+        search_args = ("--class", window["class"]) if window.get("class") else ("--name", window["name"])
+        cmd, auto_execute = window.get("command"), window.get("auto")
+    elif cls := args.class_:
+        search_args = ("--class", cls)
+        cmd, auto_execute = args.command, args.auto_execute
+    else:
+        raise ValueError
+
     focus_result = run_cmd(["xdotool", "search", "--desktop", "0", *search_args, "windowactivate"])
     if focus_result.returncode == 0:
         return
 
-    if cmd := window.get("command"):
-        if window.get("auto"):
-            run_cmd(cmd, shell=True)
+    if cmd:
+        if auto_execute:
+            Popen(cmd, shell=True)
         else:
             run_cmd(["qdbus", "org.kde.krunner", "/App", "org.kde.krunner.App.query", cmd])
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--class", dest="class_")
+    parser.add_argument("-x", "--command")
+    parser.add_argument("-y", "--auto-execute", action="store_true")
+    parser.add_argument("alias", nargs="?")
+    return parser.parse_args()
